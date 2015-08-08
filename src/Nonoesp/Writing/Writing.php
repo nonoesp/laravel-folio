35cfa3759ffac7d3928d6612d3761fefac304aa0
Nono Martínez Alonso <mundowarezweb@gmail.com>
2015-08-08 12:09:00 +0200

    Auto-commit.

--- src/Nonoesp/Writing/Writing.php
+++ src/Nonoesp/Writing/Writing.php
@@ -2,11 +2,35 @@
  
 class Writing {
  
-	public static function greeting() {
+	public static function articleCategoryClass($tags, $class) {
 
-		echo 'path: '.\Config::get('writing::path');
-		echo '<br><br>';
-		echo 'Hello, I am the Writing class. An update here.';
+		$categories = \Config::get('blog.special-tags'); //TODO: lang in package
+
+		foreach($tags as $tag) {
+			$tag = strtolower($tag);
+			if (in_array($tag, $categories)) {
+				echo "";
+				return $class.'--'.$tag.' ';
+			}
+		}
+		return;
+	}	
+
+	public static function displayArticleTags($tags, $class) {
+		$result = '';
+		$idx = 0;
+		foreach($tags as $tag) {
+			/*if($idx > 0) {
+				$result .= ', ';
+			}*/
+			$result .= Writing::articleTag($tag, $class);
+			$idx++;
+		}
+		return $result;
+	}
+
+	public static function articleTag($tag, $class) {
+		return \HTML::link(Config::get('writing::path').'/tag/'.\Conner\Tagging\TaggingUtil::slug($tag), $tag, array('class' => $class));
 	}
 
 }
