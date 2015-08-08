7dae49030fc1e5b9035046593327f82d7d44777f
Nono Martínez Alonso <mundowarezweb@gmail.com>
2015-08-08 10:56:28 +0200

    First commit.

---
+++ src/Nonoesp/Writing/WritingServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Nonoesp\Writing;
+
+use Illuminate\Support\ServiceProvider;
+
+class WritingServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('nonoesp/writing');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
