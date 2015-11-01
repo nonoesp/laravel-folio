1c10c0b771437ff8f6a5b566e8d3faf15cc72065
Nono Martínez Alonso <mundowarezweb@gmail.com>
2015-11-01 16:48:46 -0500

    Auto-commit.

--- resources/assets/js/writing.js
+++ resources/assets/js/writing.js
@@ -9,7 +9,7 @@
   });
 
   $(document).on('click', '.js-c-load-more__load-more', function(){
-  	$(".js-c-load-more").addClass('is-loading');
+    $(".js-c-load-more").addClass('is-loading');
     loadMore();
   });
 
@@ -17,31 +17,39 @@
 /* Helpers */
 
 function loadMore() {
-	if (ids.length > 0) {
+  if (ids.length > 0) {
 
-		var ids_load = [];
-		var step = 3;
+    var ids_load = [];
+    var step = 3;
 
-		for(i=0;i<step;i++) {
-			if (ids[i] == undefined) break;
-			ids_load.push(ids[i]);
-		}
+    for(i=0;i<step;i++) {
+      if (ids[i] == undefined) break;
+      ids_load.push(ids[i]);
+    }
 
-		// Load artiles and remove ids
-		articlesWithIds(ids_load);
-		ids.splice(0, step);
+    // Load artiles and remove ids
+    articlesWithIds(ids_load);
+    ids.splice(0, step);
 
     if(ids.length == 0) {
       $(".js-c-load-more__load-more").remove();
     }
-	}
+  }
 }
 
 function articlesWithIds(ids) {
+
+  // To properly set the CSRF-TOKEN with Laravel,
+  // make sure you have the following HTML meta tag in your view
+  // <meta name="csrf-token" content="{{ csrf_token() }}" />
+
   $.ajax({
         url: 'http://' + document.domain + '/articles',
         type: 'POST',
         data: {ids: ids, article_type: 'SUMMARY_ARTICLE_TYPE'},
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
         success: function(data) {
              $(".js-c-load-more").before(data);
              $(".js-c-load-more").removeClass('is-loading')
@@ -55,15 +63,15 @@ console.log('the service is ' + service);
 
   if (service == 'youtube') {
     return '<iframe width="480" height="268" style="max-width:100%"'+
-  	  	   'src="http://www.youtube.com/embed/'+code+
-  		   '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+           'src="http://www.youtube.com/embed/'+code+
+         '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
   }
 
   if (service == 'vimeo') {
     return '<iframe src="//player.vimeo.com/video/'+ code +
-    	   '?&portrait=0&title=0&byline=0&color=db4a38&badge=0&autoplay=1"'+
-    	   'width="100%" height="268" frameborder="0" webkitallowfullscreen'+
-    	   ' mozallowfullscreen allowfullscreen></iframe>';
+         '?&portrait=0&title=0&byline=0&color=db4a38&badge=0&autoplay=1"'+
+         'width="100%" height="268" frameborder="0" webkitallowfullscreen'+
+         ' mozallowfullscreen allowfullscreen></iframe>';
   }
 }
 
