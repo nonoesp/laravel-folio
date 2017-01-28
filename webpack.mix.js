f2dad4655789ff6d2efd569e70ca2556c1962dbb
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-01-28 18:21:22 -0500

    Complete assets workflow.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
 const { mix } = require('laravel-mix');
 
-mix.sass('resources/assets/sass/space.scss', 'resources/assets/css');
+mix.sass('resources/assets-dev/sass/space.scss', 'resources/assets/css')
+   .js('resources/assets-dev/js/space.js', 'resources/assets/js')
+   .sourceMaps();
