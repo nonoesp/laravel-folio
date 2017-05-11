31e240831e9a7f123cbab3c863845421aea309e6
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-05-11 15:26:13 -0400

    Fix default home template item paths.

--- resources/views/partial/c-item.blade.php
+++ resources/views/partial/c-item.blade.php
@@ -43,7 +43,7 @@
 
 	  {{-- Title --}}
 	  @if (isset($isTitleLinked))
-	    <h1>{{ Html::link(Folio::path().$item->slug, Thinker::title($item->title)) }}</h1>
+	  	<h1><a href="/{{$item->path()}}">{{ Thinker::title($item->title) }}</a></h1>
 	  @else
 	    <h1>{{ Thinker::title($item->title) }}</h1>
 	  @endif
