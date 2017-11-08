77db1a4388a02e1dbf881117e285b1c8cdce5563
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-11-08 11:49:29 -0500

    Add overridable parameter for google analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -70,7 +70,7 @@
 
 @if(!Auth::check() && config('folio.google-analytics'))
 	<!-- Google Analytics -->
-	<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{{ config('folio.google-analytics') }}', 'auto');ga('send', 'pageview');</script>
+	<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{{ $google_analytics or config('folio.google-analytics') }}', 'auto');ga('send', 'pageview');</script>
 @endif
 @yield('metadata')
 
