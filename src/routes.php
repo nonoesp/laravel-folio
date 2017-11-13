97c4670370951fd3ab81942ba96aed3aa3db83fa
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-11-13 15:35:53 -0500

    Fix routes.php

--- src/routes.php
+++ src/routes.php
@@ -22,7 +22,8 @@ use Hashids;
 /* TODO: figure out better strategy to restrict domain
  * as having an explicit domain might limit usage
  * throughout multiple domains.
- * /
+ */
+
 Route::group(['domain' => config('folio.domain'),'middleware' => Config::get("folio.middlewares")], function () {
 
 	$path = Folio::path();
