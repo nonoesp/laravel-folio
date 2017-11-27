aac004beab72548d39d52c2bbed4ddcc192d8c11
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-11-26 22:38:33 -0500

    Refactor assets publishing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-let mix = require('laravel-mix');
+let mix = require('laravel-mix').setPublicPath('resources/assets');
 
-mix.sass('resources/assets-dev/sass/folio.scss', 'resources/assets/css')
-    .js('resources/assets-dev/js/folio.js', 'resources/assets/js')
+mix.sass('resources/assets-dev/sass/folio.scss', 'nonoesp/folio/css')
+    .js('resources/assets-dev/js/folio.js', 'nonoesp/folio/js')
     .extract(['vue', 'vue-resource', 'vue-focus', 'jquery', 'validate-js', 'lodash', 'axios']);
