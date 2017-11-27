6159eb18b3c8d74f4150a595b423b3334230631c
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-11-26 20:38:15 -0500

    Add .extract() to webpack-mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const { mix } = require('laravel-mix');
 
 mix.sass('resources/assets-dev/sass/folio.scss', 'resources/assets/css')
-    .js('resources/assets-dev/js/folio.js', 'resources/assets/js');
+    .js('resources/assets-dev/js/folio.js', 'resources/assets/js')
+    .extract(['vue', 'vue-resource', 'vue-focus', 'jquery', 'validate-js', 'lodash', 'axios']);
