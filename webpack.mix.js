5e279e9e0c08f2cd633676f17d6a8d0f6b6335c6
Nono Martínez Alonso <mundowarezweb@gmail.com>
2018-10-26 15:31:59 -0400

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix').setPublicPath('resources/assets');
+const mix = require('laravel-mix').setPublicPath('resources/assets');
 
 mix.sass('resources/assets-dev/sass/folio.scss', 'nonoesp/folio/css')
     .js('resources/assets-dev/js/folio.js', 'nonoesp/folio/js')
