b077e22d1e483fe65e5db4af5ff2780559a80fa4
Nono Martínez Alonso <mundowarezweb@gmail.com>
2019-01-12 03:35:54 +0100

    Update BackupDatabase.

--- src/console/BackupDatabase.php
+++ src/console/BackupDatabase.php
@@ -8,64 +8,68 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class BackupDatabase extends Command
 {
-    protected $signature = 'db:backup';
+    protected $signature = 'db:backup {path?} {email?}';
 
 	protected $description = 'Backup the database';
 
-	protected $directory = 'backup/';
+	protected $defaultDirectory = '../backup/';
 	
     protected $process;
 	
     public function __construct()
     {
 		parent::__construct();
-	
-		$this->process = new Process(sprintf(
-			'mysqldump -u%s -p%s %s | gzip > %s',
-			env('DB_DATABASE'),
-			env('DB_USERNAME'),
-			env('DB_PASSWORD'),
-			$this->filepath()
-		));
     }
 
-	public function filename() {
-		$db_name = env('DB_DATABASE');
-		return \Date::now()->format('ymd').'_'.
-			   \Date::now()->format('His').'_'.
-				$db_name;
+	public static function constructFilepath($directory, $prefix) {
+		return $directory.'/'.$prefix.'_'.env('DB_DATABASE').'.sql.gz';
+	}
+
+	public static function timestamp() {
+		return \Date::now()->format('ymd').'_'.\Date::now()->format('His');
 	}
 
 	public function filepath() {
-		return $this->directory.$this->filename().'.sql.gz';
+		if ($this->argument('path')) {
+			return $this->argument('path');
+		}
+		return $this->constructFilepath($this->defaultDirectory, $this->timestamp());
 	}
 
     public function handle()
     {
+		$this->process = new Process(sprintf(
+			'mysqldump -u%s -p%s %s | gzip > %s',
+			env('DB_USERNAME'),
+			env('DB_PASSWORD'),
+			env('DB_DATABASE'),
+			$this->filepath()
+		));
+		
         try {
-			// return 2;
             $this->process->mustRun();
 
 			$this->info('The backup has been proceed successfully.');
 	
 			$app_url = env('APP_URL');
 			$db_name = env('DB_DATABASE');
-			$filename = $this->filename();
 			$filepath = $this->filepath();
 
-			// Send database via email
-			\Mail::send('email.text',
-			['text' =>
-				$app_url.' database <strong>'.
-				$db_name.'</strong> backed up as '.
-				'<strong>'.$filename.'.sql.gz</strong>!'
-			],
-			function ($m) use ($filepath) {
-				$m->from(config('folio.subscribers.from.email'), config('folio.subscribers.from.name'));
-				$m->to(config('folio.subscribers.to.email'), config('folio.subscribers.to.name'))
-				->subject('Database backup '.config('folio.title-short'))
-				->attach($filepath);
-			});
+			if ($this->argument('email')) {
+				\Mail::send('email.text',
+				['text' =>
+					$app_url.' database <strong>'.
+					$db_name.'</strong> backed up as '.
+					'<strong>'.$filepath.'</strong>!'
+				],
+				function ($m) use ($filepath) {
+					$m->from(config('folio.subscribers.from.email'), config('folio.subscribers.from.name'));
+					$m->to(config('folio.subscribers.to.email'), config('folio.subscribers.to.name'))
+					->subject('Database backup '.config('folio.title-short'))
+					->attach($filepath);
+				});
+			}
+
         } catch (ProcessFailedException $exception) {
             return $this->error('The backup process has been failed.');
         }
