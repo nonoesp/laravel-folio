fdd9fb0ab1302ba6b44297b733b66c8d1eb3b4ac
Nono Martínez Alonso <mundowarezweb@gmail.com>
2019-01-16 02:00:57 +0100

    Add random words to database name.

--- src/console/BackupDatabase.php
+++ src/console/BackupDatabase.php
@@ -6,6 +6,8 @@ use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
+//
+
 class BackupDatabase extends Command
 {
     protected $signature = 'folio:backup {directory?} {name?} {--email}';
@@ -14,7 +16,9 @@ class BackupDatabase extends Command
 
 	protected $defaultDirectory = '../storage/app/backup/';
 	
-    protected $process;
+	protected $process;
+	
+	protected $words = ["vegetarian", "tumble", "tidy", "terms", "beer", "motorist", "talk", "instruction", "leftovers", "discount", "concern", "promote", "wind", "gown", "flow", "touch", "storage", "stage", "axis", "computer virus", "unfair", "swop", "head", "future", "foundation", "branch", "anxiety", "bishop", "nonremittal", "package", "reality", "dump", "discover", "draft", "space", "climate", "father", "can", "burial", "post", "ladder", "cinema", "research", "vigorous", "debut", "composer", "liberal", "shift", "imagine", "amber", "still", "fool", "firm", "bold", "piano", "constituency", "transparent", "ballet", "replace", "enhance", "nun", "navy", "counter", "carrot", "spokesperson", "country", "degree", "location", "full", "bed", "earthquake", "offer", "break", "ally", "modest", "aisle", "clay", "celebration", "depart", "nail", "minor", "drop", "marine", "hardship", "discovery", "gregarious", "customer", "solution", "holiday", "underline", "constitution", "regular", "recruit", "offset", "build", "capital", "benefit", "improve", "freighter", "restaurant", "grandmother", "riot", "waist", "spot", "theme", "ordinary", "prejudice", "collection", "tie", "trail", "liberty", "experienced", "reform", "architecture", "terrify", "prevalence", "year", "will", "sweater", "forecast"];
 	
     public function __construct()
     {
@@ -64,10 +68,19 @@ class BackupDatabase extends Command
 		
         try {
             $this->process->mustRun();
-			$this->info('The backup has been successful.');
 	
 			$app_url = env('APP_URL');
 			$db_name = env('DB_DATABASE');
+
+			$attachment_name = explode("/", $filepath);
+			$random_word_a = $this->words[array_rand($this->words)];
+			$random_word_b = $this->words[array_rand($this->words)];
+			$attachment_name = str_replace(
+				".sql.gz",
+				'_'.$random_word_a.'-'.$random_word_b.".sql.gz",
+				$attachment_name[count($attachment_name)-1]
+			);
+			$this->info('Database backed up as '.$attachment_name);
 
 			if ($this->option('email')) {
 				
@@ -84,7 +97,8 @@ class BackupDatabase extends Command
 					$m->to(config('folio.subscribers.to.email'), config('folio.subscribers.to.name'))
 					->subject('Database backup '.config('folio.title-short'))
 					->attach($filepath, [
-                        'mime' => 'application/gzip',
+						'mime' => 'application/gzip',
+						'as' => $attachment_name,
 					]);
 				});
 			}
