ce8f786130b74e6bfa704f3766bb9b5627912be6
Nono Martínez Alonso <mundowarezweb@gmail.com>
2019-06-30 14:14:11 +0200

    Add Item commands.

---
+++ src/console/ItemClone.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Nonoesp\Folio\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+//
+
+class ItemClone extends Command
+{
+    protected $signature = 'folio:clone {id} {newName?}';
+
+	protected $description = 'Empty existing tags and retag all items.';
+	
+    public function __construct()
+    {
+		parent::__construct();
+    }
+
+    public function handle()
+    {
+        try {
+            // Item id
+            $id = $this->argument('id');
+            // Find item by id
+            $item = \Item::find($id);
+            // Clone if item exists
+            if ($item) {
+                $this->info('Cloning '.$item->title.'..');
+                $item->load('properties');
+                $new_item = $item->replicate();
+                if ($this->argument('newName')) {
+                    $new_item->title = $this->argument('newName');
+                } else {
+                    $new_item->title = $new_item->title.' (copy)';
+                }
+                $new_item->save();
+        
+                foreach($item->properties as $property) {
+                    $new_property = $property->replicate();
+                    $new_property->item_id = $new_item->id;
+                    $new_property->save();
+                    $this->info(' · '.$property->label.' · '.$property->name.' · '.$property->value);
+                }
+            } else {
+                $this->error('There\'s no item '.$id);
+            }
+
+        } catch (ProcessFailedException $exception) {
+            return $this->error('Cloning this item failed.');
+        }
+    }
+}
