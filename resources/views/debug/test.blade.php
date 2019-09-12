66d2a7ff9f67835727202a53a32965e592afdae9
Nono Martínez Alonso <mundowarezweb@gmail.com>
2019-09-12 19:54:50 +0200

    Add test functions.

--- resources/views/debug/test.blade.php
+++ resources/views/debug/test.blade.php
@@ -1,3 +1,16 @@
+<style>
+* {
+  font-family: 'Inter', system-ui, sans-serif;
+  font-size: 0.95rem;
+}
+
+.comment {
+  color: rgba(0,0,0,0.4);
+}
+</style>
+
+<h2>Folio debug</h2>
+
 <h3>nonoesp/thinker</h3>
 {{ Thinker::title('My Name Is Peter I Have An Ipad') }}
 <br>
@@ -35,3 +48,42 @@
   {{ Html::link('e/'.Hashids::encode($item->id), 'e/'.Hashids::encode($item->id)) }} ({{ $item->title }})
   <br>
 @endforeach
+
+<h3>spatie/laravel-translatable</h3>
+
+<?php
+  $item = new Item();
+  $item->setTranslation('title', 'en', 'This weird item');
+  $item->setTranslation('title', 'es', 'Este raro objeto');
+?>
+
+@php App::setLocale('en') @endphp
+English [en] · {{ $item->title }}
+<span class="comment">// This weird item</span>
+<br>
+@php App::setLocale('es') @endphp
+Spanish [es] · {{ $item->title }}
+<span class="comment">// Este raro objeto</span>
+<br>
+@php App::setLocale('it') @endphp
+Italian [it] · {{ $item->title }}
+<span class="comment">// fallback to 'en'</span>
+
+@php App::setLocale('en') @endphp
+
+<h3>Item</h3>
+
+<?php
+  $item = Item::withTrashed()->orderBy('published_at', 'DESC')->first();
+?>
+
+title · {{ $item->text }}
+<br>
+<br>
+edit path · {{ $item->editPath() }}
+<br>
+url · {{ $item->URL() }}
+<br>
+share path · {{ $item->sharePath() }}
+<br>
+encoded path · {{ $item->encodedPath() }}
