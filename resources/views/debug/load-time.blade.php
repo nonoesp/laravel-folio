fc5966c5ee8272739722f82da365d35de3a27709
Nono Martínez Alonso <mundowarezweb@gmail.com>
2020-03-05 17:00:54 +0100

    Include truncate function.

--- resources/views/debug/load-time.blade.php
+++ resources/views/debug/load-time.blade.php
@@ -1,3 +1,13 @@
+@php
+    function truncate($val, $f="0")
+    {
+        if(($p = strpos($val, '.')) !== false) {
+            $val = floatval(substr($val, 0, $p + 1 + $f));
+        }
+        return $val;
+    }
+@endphp
+
 <style>
 * {
     text-align:center;
