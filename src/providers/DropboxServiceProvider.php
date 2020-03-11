ddd60f475c195d2f30bb9fe2b1cdc57cdf0d34cb
Nono Martínez Alonso <mundowarezweb@gmail.com>
2020-03-11 10:24:42 +0100

    Add DropboxServiceProvider.

---
+++ src/providers/DropboxServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Nonoesp\Folio\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Spatie\Dropbox\Client as DropboxClient;
+use Spatie\FlysystemDropbox\DropboxAdapter;
+use Storage;
+
+class DropboxServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('dropbox', function ($app, $config) {
+            $client = new DropboxClient(
+                $config['authorization_token']
+            );
+
+            return new Filesystem(new DropboxAdapter($client));
+        });
+    }
+}
