256daa39acf8133a77d78eb5d7b5ef8a3f2ffc2a
Nono Martínez Alonso <mundowarezweb@gmail.com>
2020-03-11 22:10:30 +0100

    Cleanup /time view.

--- resources/views/debug/time.blade.php
+++ resources/views/debug/time.blade.php
@@ -1,38 +1,42 @@
 @extends('folio::template._base')
 
 <?php
-$site_title = 'Time · '.config('folio.title');
-$og_title = $site_title;
-// header
-$header_view = 'folio::partial.c-header-simple';
-$header_data = [
-  'image' => null,
-  'is_media_hidden' => true,
-];
-$header_classes = ['borderless'];
-$cover_hidden = true;
+  $site_title = 'Time · '.config('folio.title');
+  $og_title = $site_title;
+  // header
+  $header_view = config('folio.header.view');
+  $header_data = [
+    'image' => null,
+    'is_media_hidden' => true,
+  ];
+  $header_classes = ['borderless'];
+  $cover_hidden = true;
 
-$timezone = config('app.timezone');
-$date = Date::now();
-$date = ucWords($date->format('F').' '.$date->format('j, Y').$date->format(' H:i:s'));
+  $timezone = config('app.timezone');
+  $date = Date::now()->format('F j, Y H:i:s');
 ?>
 
 @section('content')
 	
-	<div class="[ o-wrap o-wrap--size-500 ]">
+	<div class="[ o-wrap o-wrap--full ]" style="position:fixed;left:0;right:0;top:0;bottom:0;margin:auto;height:300px;">
 
-    	<article class="[ c-item-v2 ]" style="font-size:1.1em">
+    	<article class="[ c-item-v2 ]" style="font-size:1.4rem">
 
 			<p class="[ u-text-align--center statement tk-myriad-pro ]">
-			What's the time at Nono.MA?</strong>
-			<br>
-			<br>
-			{{ $date }}
-			<span class="u-opacity--high">{{ $timezone }}</span>
+        What's the time at {!! str_replace(' ', '&nbsp;', config('folio.title-short')) !!}?
+			  <br/>
+			  <br/>
+			  {{ $date }}
+			  <span class="u-opacity--high">
+          {{ $timezone }}
+        </span>
 			</p>
 
 		</article>
 
 	</div>
 
+@stop
+
+@section('footer')
 @stop
