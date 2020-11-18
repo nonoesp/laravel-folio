73cb81a93dcb7180e381039aff04fa79c544c5ce
Nono Martínez Alonso <mundowarezweb@gmail.com>
2020-11-18 21:43:21 +0100

    Add support for Google Analytics 4.

--- resources/views/layout-v2.blade.php
+++ resources/views/layout-v2.blade.php
@@ -124,8 +124,14 @@
 
 @if(!$admin && $google_analytics)
     <!-- Google Analytics -->
+@if(Str::of($google_analytics)->startsWith('UA-'))
 	<link rel="dns-prefetch" href="//www.google-analytics.com">
-	<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{{ $google_analytics }}', 'auto');ga('send', 'pageview');</script>
+    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{{ $google_analytics }}', 'auto');ga('send', 'pageview');</script>
+@else
+	<link rel="dns-prefetch" href="//www.googletagmanager.com">
+	<script async src="https://www.googletagmanager.com/gtag/js?id={{ $google_analytics }}"></script>
+	<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '{{ $google_analytics }}');</script>	
+@endif
 @endif
 
 @if($google_fonts)
