4ede5db34c1a375083a8c1aa146f332be635fa3e
Nono Martínez Alonso <mundowarezweb@gmail.com>
2021-01-05 10:04:25 +0100

    Add Google Analytics Tag 3 to legacy layout.

--- resources/views/layout/plain.blade.php
+++ resources/views/layout/plain.blade.php
@@ -81,10 +81,18 @@
 	<!-- RSS -->
 	<link rel="alternate" type="application/atom+xml" href="/{{ config('folio.feed.route') }}" />
 
-@if(!Auth::check() && config('folio.google-analytics'))
-	<!-- Google Analytics -->
-	<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{{ $google_analytics ?? config('folio.google-analytics') }}', 'auto');ga('send', 'pageview');</script>
+@if(!$admin && $google_analytics)
+    <!-- Google Analytics -->
+@if(Str::of($google_analytics)->startsWith('UA-'))
+	<link rel="dns-prefetch" href="//www.google-analytics.com">
+    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{{ $google_analytics }}', 'auto');ga('send', 'pageview');</script>
+@else
+	<link rel="dns-prefetch" href="//www.googletagmanager.com">
+	<script async src="https://www.googletagmanager.com/gtag/js?id={{ $google_analytics }}"></script>
+	<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '{{ $google_analytics }}');</script>	
 @endif
+@endif
+
 @yield('metadata')
 
 </head>
