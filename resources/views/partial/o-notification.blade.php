2dcb3094e57cb48562609b070bc042e139012487
Nono Martínez Alonso <mundowarezweb@gmail.com>
2021-04-20 12:38:02 +0200

    Str › Illuminate\Support

--- resources/views/partial/o-notification.blade.php
+++ resources/views/partial/o-notification.blade.php
@@ -3,7 +3,7 @@
     $classes = $classes ?? [];
     // Expand modifier classes
     foreach($classes as $key => $class) {
-      if (Str::of($class)->startsWith('--')) {
+      if (Illuminate\Support\Str::of($class)->startsWith('--')) {
         $classes[$key] = $notification_class.$class;
       }
     }
