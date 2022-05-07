2a3484c442d57fd0fee12404fe564ddadfe91a15
Nono Martínez Alonso <mundowarezweb@gmail.com>
2022-05-08 00:00:29 +0200

    Upgrade Dropbox/Spaces service providers.

--- src/Providers/DropboxServiceProvider.php
+++ src/Providers/DropboxServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace Nonoesp\Folio\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider as ServiceProvider;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
 use League\Flysystem\Filesystem;
 use Spatie\Dropbox\Client as DropboxClient;
 use Spatie\FlysystemDropbox\DropboxAdapter;
-use Storage;
 
 class DropboxServiceProvider extends ServiceProvider
 {
@@ -27,12 +28,26 @@ class DropboxServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Storage::extend('dropbox', function ($app, $config) {
-            $client = new DropboxClient(
-                $config['authorization_token']
-            );
+        // 8.x
+        // Storage::extend('dropbox', function ($app, $config) {
+        //     $client = new DropboxClient(
+        //         $config['authorization_token']
+        //     );
 
-            return new Filesystem(new DropboxAdapter($client));
+        //     return new Filesystem(new DropboxAdapter($client));
+        // });
+
+        // 9x
+        Storage::extend('dropbox', function ($app, $config) {
+            $adapter = new DropboxAdapter(
+                new DropboxClient($config['authorization_token'])
+            );
+         
+            return new FilesystemAdapter(
+                new Filesystem($adapter, $config),
+                $adapter,
+                $config
+            );
         });
     }
 }
--- src/Providers/SpacesServiceProvider.php
+++ src/Providers/SpacesServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace Nonoesp\Folio\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider as ServiceProvider;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
 use League\Flysystem\Filesystem;
 use Aws\S3\S3Client;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
-use Storage;
+use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
 
 /**
  * Digital Ocean Spaces
@@ -30,6 +31,24 @@ class SpacesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // 8.x
+        // Storage::extend('spaces', function ($app, $config) {
+        //     $client = new S3Client([
+        //         'credentials' => [
+        //             'key'    => $config['key'],
+        //             'secret' => $config['secret'],
+        //         ],
+        //         'region' => $config['region'],
+        //         'version' => 'latest',
+        //         'endpoint' => $config['endpoint'],
+        //     ]);
+
+        //     $adapter = new AwsS3Adapter($client, $config['bucket']);
+
+        //     return new Filesystem($adapter);
+        // });
+
+        // 9.x
         Storage::extend('spaces', function ($app, $config) {
             $client = new S3Client([
                 'credentials' => [
@@ -41,9 +60,13 @@ class SpacesServiceProvider extends ServiceProvider
                 'endpoint' => $config['endpoint'],
             ]);
 
-            $adapter = new AwsS3Adapter($client, $config['bucket']);
-
-            return new Filesystem($adapter);
+            $adapter = new AwsS3V3Adapter($client, $config['bucket']);
+         
+            return new FilesystemAdapter(
+                new Filesystem($adapter, $config),
+                $adapter,
+                $config
+            );
         });
     }
 }
