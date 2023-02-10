e9279de7d70354b06b8cf024e4a675351cef75d7
Nono Martínez Alonso <mundowarezweb@gmail.com>
2023-02-10 13:26:34 +0100

    Support Google Analytics 4.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -134,7 +134,7 @@ $google_analytics = config('folio.google-analytics');
 @endphp
 @if(!$admin && $google_analytics)
     <!-- Google Analytics -->
-@if(Str::of($google_analytics)->startsWith('UA-'))
+@if(\Str::of($google_analytics)->startsWith('UA-'))
 	<link rel="dns-prefetch" href="//www.google-analytics.com">
     <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '{{ $google_analytics }}', 'auto');ga('send', 'pageview');</script>
 @else
